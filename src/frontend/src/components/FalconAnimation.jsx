3ed8eb7521e95821e2c232704cb8f064f7d6cad3
Sergio Mendoza <sergio_mendoza78@yahoo.com>
2026-04-26 15:37:55 -0500

    added falcon animaiton and updated landing page image

--- src/frontend/src/components/FalconAnimation.jsx
+++ src/frontend/src/components/FalconAnimation.jsx
@@ -1,15 +1,27 @@
+import React from "react";
 import "./FalconAnimation.css";
 import falconImage from "../assets/flying_falcon.png";
 
 function FalconAnimation() {
   return (
     <div className="falcon-animation">
-      <img
-        src={falconImage}
-        alt="Blue Falcon flying"
-        className="falcon-img"
-        draggable={false}
-      />
+      <div className="falcon falcon-1">
+        <div className="falcon-wing-motion">
+          <img src={falconImage} className="falcon-img" alt="Falcon" />
+        </div>
+      </div>
+
+      <div className="falcon falcon-2">
+        <div className="falcon-wing-motion">
+          <img src={falconImage} className="falcon-img" alt="Falcon" />
+        </div>
+      </div>
+
+      <div className="falcon falcon-3">
+        <div className="falcon-wing-motion">
+          <img src={falconImage} className="falcon-img" alt="Falcon" />
+        </div>
+      </div>
     </div>
   );
 }
